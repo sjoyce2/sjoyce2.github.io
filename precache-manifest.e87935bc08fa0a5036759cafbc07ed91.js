72a6e2af067799701a4f7c8c6894bbc142558a4c
sjoyce2 <sjoyce2@zagmail.gonzaga.edu>
2020-08-26 14:16:32 -0700

    Updates

---
+++ precache-manifest.e87935bc08fa0a5036759cafbc07ed91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7f9d7d92ad4992d581041bc87c8f37c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ca5a402de8d05f20020",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7f7506d485d96d436dd5",
+    "url": "/static/js/2.4cbe87e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4cbe87e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ca5a402de8d05f20020",
+    "url": "/static/js/main.1a84215d.chunk.js"
+  },
+  {
+    "revision": "ba126735810163618457",
+    "url": "/static/js/runtime-main.3374ac37.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
