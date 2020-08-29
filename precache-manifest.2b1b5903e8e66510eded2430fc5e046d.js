40800cfc2c616b1756ddd0ef25c48550b7e9d164
sjoyce2 <sjoyce2@zagmail.gonzaga.edu>
2020-08-28 17:01:22 -0700

    Updates

---
+++ precache-manifest.2b1b5903e8e66510eded2430fc5e046d.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f3a61f3a647502c8842d2bf3ae51d23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aef050e51171614707e3",
+    "url": "/static/css/main.bd985b52.chunk.css"
+  },
+  {
+    "revision": "a53e0809a1bde605aaf9",
+    "url": "/static/js/2.5d8f108f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5d8f108f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aef050e51171614707e3",
+    "url": "/static/js/main.0960cfc7.chunk.js"
+  },
+  {
+    "revision": "ba126735810163618457",
+    "url": "/static/js/runtime-main.3374ac37.js"
+  },
+  {
+    "revision": "5c74b608d675801d9db2a074ce689140",
+    "url": "/static/media/background.5c74b608.jpg"
+  },
+  {
+    "revision": "35e45730b26779243daca0288aa5c155",
+    "url": "/static/media/cpp.35e45730.jpg"
+  },
+  {
+    "revision": "2807ea9fe4b4db99ff92901069b8fe53",
+    "url": "/static/media/cs.2807ea9f.png"
+  },
+  {
+    "revision": "c26a73b45b9a801864f13edeb0aa2cfb",
+    "url": "/static/media/css3.c26a73b4.png"
+  },
+  {
+    "revision": "d991e2f9a54a1c233f71769a4e9309cc",
+    "url": "/static/media/github.d991e2f9.png"
+  },
+  {
+    "revision": "664a8c00ef526bf33edf55808601d461",
+    "url": "/static/media/html5.664a8c00.png"
+  },
+  {
+    "revision": "dc4b52b1ae1695ceb7eb495f723217b6",
+    "url": "/static/media/java.dc4b52b1.png"
+  },
+  {
+    "revision": "0cded3a3276425911d55a2552bf361bf",
+    "url": "/static/media/javascript.0cded3a3.png"
+  },
+  {
+    "revision": "ecc6f9e5d804ae0db7490c683af2f406",
+    "url": "/static/media/mongodb.ecc6f9e5.png"
+  },
+  {
+    "revision": "754e01e49836a295574661a188161775",
+    "url": "/static/media/mysql.754e01e4.png"
+  },
+  {
+    "revision": "ed81f981667f8a576d27308f5fdc6820",
+    "url": "/static/media/net.ed81f981.png"
+  },
+  {
+    "revision": "022448b031661b895910b00a7a1d29a5",
+    "url": "/static/media/profile.022448b0.jpg"
+  },
+  {
+    "revision": "b9bbb1d722d3ef6ebf7d573703b5925e",
+    "url": "/static/media/python.b9bbb1d7.png"
+  },
+  {
+    "revision": "7eae888d18e639ccab1e0d7c66fe8b19",
+    "url": "/static/media/sql.7eae888d.png"
+  }
+]);
